5f8986b6a9145fb881c1fef5131f77ffd0f50ac5
DESKTOP-P2E4GPS\505 <hexun_@naver.com>
2022-08-04 18:00:10 +0900

    2022_08_04 React

---
+++ styling-react/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import StyledComponent from './StyledComponent';
+class App extends Component {
+  render() {
+    return <StyledComponent />;
+  }
+}
+
+export default App;
